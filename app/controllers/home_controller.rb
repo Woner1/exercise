d08b8bbedd919dd52733ebc6d6bdadeb93f3c5cf
Ankh <2609746800@qq.com>
2018-08-27 16:17:21 +0800

    master

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,22 @@
 class HomeController < ApplicationController
+  PRODUCT_COUNT=100
+  LIMITED_PRODUCT_NUMBER=20
   def index
-    @nav=false
+    if params[:page]
+    @page=(params[:page].to_i)
+    else
+      @page=1
+      end
+    @last_page=(PRODUCT_COUNT/LIMITED_PRODUCT_NUMBER)
+    @fist_page=1
+    @product=[]
+    (1..PRODUCT_COUNT).each do |i|
+  index={
+        :number=>"#{i}"
+  }
+
+  @product.push(index)
+    end
+    @product=@product[ (@page-1) * LIMITED_PRODUCT_NUMBER,LIMITED_PRODUCT_NUMBER]
   end
 end
